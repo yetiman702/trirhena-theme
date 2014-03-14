06ddf58ce080b1b31dc932662ffd0bc746225f05
Moritz Hoffmann <scutze@freenet.de>
2014-03-14 01:08:50 +0100

    removed site branding

--- header.php
+++ header.php
@@ -33,11 +33,6 @@ error_reporting(E_ALL);
 			</div>
 		</a>
 	</header><!-- /#titlebar -->
-	
-	<div class="site-branding">
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-	</div>
 
 	<div id="window">
 		<div id="header-img">
