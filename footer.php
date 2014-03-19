c98950580ac3c49aa9d55e94427d671dea55c880
Moritz Hoffmann <scutze@freenet.de>
2014-03-19 21:50:21 +0100

    removed imprint

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'trirhena_theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'trirhena_theme' ), 'trirhena_theme', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php /* printf( __( 'Theme: %1$s by %2$s.', 'trirhena_theme' ), 'trirhena_theme', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); */ ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
