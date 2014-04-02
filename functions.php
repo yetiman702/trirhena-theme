ef7a8cb482746c2b3b5b6da9beef48a4028cdd95
Jan Vogt <news.jan.vogt@me.com>
2014-04-02 21:50:15 +0200

    implement menu-animation

--- functions.php
+++ functions.php
@@ -87,6 +87,8 @@ function trirhena_theme_scripts() {
 
 	wp_enqueue_script( 'trirhena_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'trirhena_theme_menu_animation', get_template_directory_uri() .  '/js/menu-animation.js', array('jquery'));
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
