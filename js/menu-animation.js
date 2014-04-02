e7fea57f492e3c392674fbe6d636a54f8eab4b68
Jan Vogt <news.jan.vogt@me.com>
2014-04-02 22:32:00 +0200

    refactor menu animation

--- js/menu-animation.js
+++ js/menu-animation.js
@@ -6,20 +6,16 @@
 		var nav_menu = $( "#site-navigation" );
 		var branding = $( "#site-branding" );
 		if(nav_menu.css("position") == "absolute" && scroll_offset > nav_menu.offset().top){
-			nav_menu.css("position", "fixed");
-			nav_menu.css("top", "0px");
+			nav_menu.css({"position": "fixed", "top": "0px"});
 		}
 		else if(nav_menu.css("position") == "fixed" && scroll_offset < branding.offset().top + branding.outerHeight()){
-			nav_menu.css("position", "absolute");
-			nav_menu.css("top", "");
+			nav_menu.css({"position": "absolute", "top": ""});
 		}
 		if(branding.css("position") == "fixed" && scroll_offset > nav_menu.offset().top - branding.outerHeight()){
-			branding.css("position", "absolute");
-			branding.css("bottom", nav_menu.outerHeight() + "px");
+			branding.css({"position": "absolute", "bottom": nav_menu.outerHeight() + "px"});
 		}
 		else if(branding.css("position") == "absolute" && scroll_offset < branding.offset().top) {
-			branding.css("position", "fixed");
-			branding.css("bottom", "");
+			branding.css({"position": "fixed", "bottom": ""});
 		}
 		if(scroll_offset < nav_menu.offset().top - padding_anim_range) {
 			nav_menu.css("padding-left", 0);
@@ -28,7 +24,7 @@
 			nav_menu.css("padding-left", parseInt($( "#site-branding" ).css("padding-left")));
 		}
 		else{
-			factor = (scroll_offset - nav_menu.offset().top + padding_anim_range) / padding_anim_range;
+			var factor = (scroll_offset - nav_menu.offset().top + padding_anim_range) / padding_anim_range;
 			nav_menu.css("padding-left", factor * parseInt($( "#site-branding" ).css("padding-left")));
 		}
 	});
