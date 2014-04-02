2c73ce4428718bb95c92c4ce3a6e42a1fc74beb2
Moritz Hoffmann <scutze@freenet.de>
2014-04-02 08:07:12 +0200

    removed sidebar where unwanted

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area onecolumn">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -31,5 +31,6 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
+<?php /* get_sidebar(); // No sidebar for page display */ ?>
 <?php get_footer(); ?>
