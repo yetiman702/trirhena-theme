815c8d9aff335b14e1de90ead662d6638de47cce
Moritz Hoffmann <scutze@freenet.de>
2014-04-02 15:56:04 +0200

    styled time stamp

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -82,8 +82,8 @@ function trirhena_theme_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'trirhena_theme' ),
-		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
+	printf( __( '<span class="posted-on">%1$s</span><span class="byline"> by %2$s</span>', 'trirhena_theme' ),
+		sprintf( '<a href="%1$s" rel="bookmark" class="time">%2$s</a>',
 			esc_url( get_permalink() ),
 			$time_string
 		),
